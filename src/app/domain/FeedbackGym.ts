ecaa80c3bf6dbc4374b35c1bee932b4841a04407
AposVinc <apos.vincenzo@gmail.com>
2021-01-16 16:00:48 +0100

    update gyms reviews

--- src/app/domain/FeedbackGym.ts
+++ src/app/domain/FeedbackGym.ts
@@ -2,5 +2,9 @@ export interface FeedbackGym {
   id: number;
   feed: string;
   rating: number;
+  user: number;
+  userName: string;
+  userLastname: string;
   gym: number;
+  gymName: string;
 }
